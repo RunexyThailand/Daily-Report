1a451d34f6b1c49fd2fc55b3da5d9f7d90e449af
KittipongM <kittipong-m@runexy.co.jp>
2025-10-24 13:23:49 +0700

    console error

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
+
 const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin({});
-
 const nextConfig: NextConfig = {
-  output: "standalone",
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  images: {
-    unoptimized: true, // ถ้า export แบบไฟล์ล้วนก็พร้อม
-    remotePatterns: [],
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
