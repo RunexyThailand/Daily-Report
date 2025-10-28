34db70aef118510e245cfad498f4559eba356bff
Paramet-L <paramet-l@runexy.co.jp>
2025-10-28 14:55:23 +0700

    profile page

---
+++ src/app/(protected)/profile/page.tsx
@@ -0,0 +1,13 @@
+"use server";
+
+import ProfileClient from "./client";
+
+const ProfilePage = async () => {
+  return (
+    <div>
+      <ProfileClient />
+    </div>
+  );
+};
+
+export default ProfilePage;
