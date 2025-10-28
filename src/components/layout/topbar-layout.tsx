e8db9d8399e33968f64178d7b67d2bfe5a6ecfe4
Paramet-L <paramet-l@runexy.co.jp>
2025-10-28 15:26:46 +0700

    test pr

--- src/components/layout/topbar-layout.tsx
+++ src/components/layout/topbar-layout.tsx
@@ -54,7 +54,10 @@ export function Topbar({ className }: { className?: string }) {
   return (
     <header className={cn("sticky top-0 z-40 w-full bg-[#234868]", className)}>
       <div className="flex justify-between items-center">
-        <div className="flex h-14 items-center gap-2 px-4">
+        <div
+          className="flex h-14 items-center gap-2 px-4"
+          data-testid="topbar-logo"
+        >
           {/* <Button
             variant="ghost"
             size="icon"
